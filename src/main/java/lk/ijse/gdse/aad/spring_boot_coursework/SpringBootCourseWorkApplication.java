6d3ef0aee910e76d27ffa4bbe0db613f82fde1ef
rayan <rayancooray123456@gmail.com>
2024-04-18 15:00:54 +0530

    Spring Boot Coursework Base Configuration setup

---
+++ src/main/java/lk/ijse/gdse/aad/spring_boot_coursework/SpringBootCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad.spring_boot_coursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCourseWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCourseWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/lk/ijse/gdse/aad/spring_boot_coursework/SpringBootCourseWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.aad.spring_boot_coursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCourseWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
