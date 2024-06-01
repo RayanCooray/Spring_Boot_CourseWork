1ea9c8f27a158760c0ff337e21a353cdec72dfea
rayan <rayancooray123456@gmail.com>
2024-06-01 20:48:52 +0530

    InventorySetController with gender generation changed due to custom id

--- src/main/java/lk/ijse/gdse/aad/spring_boot_coursework/entity/MenWomenItem.java
+++ src/main/java/lk/ijse/gdse/aad/spring_boot_coursework/entity/MenWomenItem.java
@@ -1,10 +1,13 @@
 package lk.ijse.gdse.aad.spring_boot_coursework.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import lk.ijse.gdse.aad.spring_boot_coursework.IdGen.CustomIdGenerator;
 
 import java.util.List;
 
@@ -15,10 +18,10 @@ import java.util.List;
 @Table (name = "gender")
 public class MenWomenItem {
     @Id
-    @Nullable()
     private String genderCode;
     private String genderDesc;
     @OneToMany(mappedBy = "genderEntity",cascade = CascadeType.ALL)
+    @JsonIgnore
     private List<Item> itemEntities;
 
 }
